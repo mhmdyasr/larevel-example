32622552d33713edb6d4d3984ce0e8885d5adf73
Muhammed Yasir <muhammedyasirktg@gmail.com>
2023-12-01 17:50:33 +0530

    ready

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Home Page</title>
+  </head>
+  <body>
+    sample page created to understand how routes/web.php works <br>
+    <h1>Welcome home!</h1>
+  </body>
+</html>
